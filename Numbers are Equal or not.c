6d8ddae276874e148cb0803b7c1093e7fb217625
Anitha-K2006 <anitha3112kumar@gmail.com>
2024-10-06 05:51:49 -0700

    Create Numbers are Equal or not.c

---
+++ Numbers are Equal or not.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int num1,num2;
+    scanf("%d",&num1);
+    scanf("%d",&num2);
+    if(num1==num2)
+    {
+        printf("The Two Numbers are Equal");
+    }
+    else
+    {
+        printf("The Two Numbers are not Equal");
+    }
+    return 0;
+}
